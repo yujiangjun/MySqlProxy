8df5158a6a2abe76dde99a642742a973a1b64df5
余江俊 <18512106361@163.com>
2021-10-14 20:18:26 +0800

    11

---
+++ main1.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"net"
+	"time"
+)
+
+func main() {
+	var buf [512]byte
+	addr, err := net.ResolveTCPAddr("tcp", "192.168.132.129:3306")
+	result := checkError(err)
+	if !result {
+		return
+	}
+	conn, err := net.DialTCP("tcp", nil, addr)
+	result = checkError(err)
+	if !result {
+		return
+	}
+	remoteAddr := conn.RemoteAddr()
+	readByte, err := conn.Read(buf[0:])
+	result = checkError(err)
+	if !result {
+		return
+	}
+	fmt.Println("Reply from Server", remoteAddr.String(), string(buf[0:readByte]))
+	fmt.Println(buf)
+	dstEncode := make([]byte, hex.EncodedLen(len(buf)))
+	hex.Encode(dstEncode, buf[0:readByte])
+	fmt.Println("16进制")
+	fmt.Printf("%x\n", dstEncode)
+
+	fmt.Println("dump")
+	fmt.Println(hex.Dump(buf[0:readByte]))
+	time.Sleep(time.Second * 2)
+
+	conn.Close()
+}
+
+func checkError(err error) bool {
+	if err != nil {
+		fmt.Errorf("fatal error:%s", err)
+		return false
+	}
+	return true
+}
